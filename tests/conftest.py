f5eb0770eef280e48547e6491999ddb0d9b6ea72
Christopher Burrows <99897839+cbradiodrums@users.noreply.github.com>
2023-03-08 14:26:28 -0500

    Testing Sprint 1 Builds

---
+++ tests/conftest.py
@@ -0,0 +1,34 @@
+from os import getenv
+import tempfile
+import pytest
+
+from flask import Flask, render_template, request
+
+APP = Flask(__name__)
+
+
+@pytest.fixture
+def app():
+    """Create and configure a new app instance for each test."""
+    # create a temporary file to isolate the database for each test
+    db_fd, db_path = tempfile.mkstemp()
+    # create the app with common test config
+    app = APP
+
+    # create the database and load test data
+    with app.app_context():
+        ...
+
+    yield app
+
+
+@pytest.fixture
+def client(app):
+    """A test client for the app."""
+    return app.test_client()
+
+
+@pytest.fixture
+def runner(app):
+    """A test runner for the app's Click commands."""
+    return app.test_cli_runner()
